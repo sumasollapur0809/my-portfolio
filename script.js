95a28d2a6fb2bd848a419be1e8b627e717075283
suma <suma@gmail.com>
2026-03-29 01:03:02 +0530

    toggle effect added to darkmode button

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // change icon
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+});
