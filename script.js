af731e9b1d6d234fdadf11f39b1a5c9718819865
suma <suma@gmail.com>
2026-03-30 09:57:30 +0530

    style js html changed

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-const toggleBtn = document.getElementById("theme-toggle");
+const toggleBtns = document.querySelectorAll("#theme-toggle");
 
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+toggleBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
 
-    // change icon
-    if (document.body.classList.contains("dark-mode")) {
-        toggleBtn.textContent = "☀️";
-    } else {
-        toggleBtn.textContent = "🌙";
-    }
+        toggleBtns.forEach(b => {
+            if (document.body.classList.contains("dark-mode")) {
+                b.textContent = "☀️";
+            } else {
+                b.textContent = "🌙";
+            }
+        });
+    });
 });
