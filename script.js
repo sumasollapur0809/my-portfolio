fe5f1abf201f36fc6979daf9e5e5153e5544c00b
suma sollapur <sumasollapur0908@gmail.com>
2026-04-04 08:26:41 +0000

    chatbot added

--- script.js
+++ script.js
@@ -20,3 +20,51 @@ toggleBtns.forEach(btn => {
         });
     });
 });
+
+// for chatbot
+function toggleChat() {
+  let box = document.getElementById("chatBox");
+  box.style.display = box.style.display === "block" ? "none" : "block";
+}
+function sendMessage() {
+  let input = document.getElementById("userInput");
+  let message = input.value.toLowerCase();
+  let chatBody = document.getElementById("chatBody");
+  if (message.trim() === "") return;
+  // Show user message
+  chatBody.innerHTML += `<div class="user">${message}</div>`;
+  let reply = getBotReply(message);
+  // Show bot reply
+  setTimeout(() => {
+    chatBody.innerHTML += `<div class="bot">${reply}</div>`;
+    chatBody.scrollTop = chatBody.scrollHeight;
+  }, 500);
+  input.value = "";
+}
+function getBotReply(msg) {
+  if (msg.includes("hello") || msg.includes("hi")) {
+    return "Hello 😊! How can I help you?";
+  }
+  if (msg.includes("name") || msg.includes("who are you")) {
+    return "I am a chatbot 🤖 created to assist you with information about me and my work.";
+  }
+  if (msg.includes("contact") || msg.includes("email")) {
+    return "connect with us at : sumasollapur0908@gmail.com";
+  }
+  if (msg.includes("contact") || msg.includes("number")) {
+    return "contact us at : +91 9960795912";
+  }
+  if (msg.includes("project")) {
+    return "You can check my projects in the Projects section of this website.";
+  }
+  if (msg.includes("thank you") || msg.includes("thanks")) {
+    return "You're welcome! If you have any more questions, feel free to ask.";
+  }
+  return "Sorry, I didn’t understand. Ask about contact, projects, or services.";
+}
+document.getElementById("userInput").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); 
+    sendMessage(); 
+  }
+});
